2029555136595cd5aae964f447f89f8fad7d21b7
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-16 16:39:52 +0200

    Stampa della lista nel dom

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,46 @@
+// Creo elemento all' interno del div con classe container
+// 1 - Seleziono il container
+const div = document.querySelector("div.container");
+
+// 2 -Creo elemnti nel container
+
+const ulFizzBuzz = document.createElement("ul")
+
+const liFizzBuzz = document.createElement("li")
+
+console.log(typeof(liFizzBuzz))
+
 // Fai una lista che arrivi fino a 100 e ad ogni numero controlla per cosa è divisibile
 
 for( let index=1 ; index <= 100; index++){
+    
     if((index % 3 === 0) && (index % 5 === 0)){
         console.log("FizzBuzz");
+        div.append(ulFizzBuzz.append(liFizzBuzz));
+        liFizzBuzz.append("FizzBuzz")
     }else if(index % 3 === 0){
         console.log("Fizz");
+        div.append(ulFizzBuzz.append(liFizzBuzz));
+        liFizzBuzz.append("Fizz")
     }else if(index % 5 === 0){
         console.log("Buzz");
+        div.append(ulFizzBuzz.append(liFizzBuzz));
+        liFizzBuzz.append("Buzz")
     }else{
         console.log(index);
+        div.append(ulFizzBuzz.append(liFizzBuzz));
+        liFizzBuzz.append(index)
     }
+
      
 }
 
-// Creo elemento all' interno del div con classe container
+console.log(typeof(liFizzBuzz))
 
-const div = document.querySelector("div.container");
 
-// 1) creo elemento in memoria
 
-const bigTitle = document.createElement("h1");
 
-// 2) Lo popolo
 
-bigTitle.append("Soda stream");
 
-//3) Lo aggiungo nel Dom in un elemnto già esistente
-
-div.append(bigTitle);
 
 
